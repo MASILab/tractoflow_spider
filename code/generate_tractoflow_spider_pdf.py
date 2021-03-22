92bf747a085b4d33c14c17d9bdd8d86c92fa2c46
frheault <francois.m.rheault@usherbrooke.ca>
2021-03-22 17:14:29 -0500

    Working without --contain option

--- code/generate_tractoflow_spider_pdf.py
+++ code/generate_tractoflow_spider_pdf.py
@@ -1,13 +1,88 @@
 #!/usr/bin/env python
-
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from fpdf import FPDF
 import sys
 
-from rheault_research.utils import pdf as pdf_creator
 
-html_info = pdf_creator.parse_report('report.html')
+def parse_report(filename):
+    f = open(filename, "r")
+
+    lines = f.readlines()
+    final = []
+    for i, line in enumerate(lines):
+        if 'Run times' in line:
+            tmp_1, tmp_2 = lines[i+2].split(' - ')
+            tmp_1 = tmp_1.replace('<span id="workflow_start">', '')
+            tmp_1 = tmp_1.replace('</span>', '').strip()
+            tmp_2 = tmp_2.replace('<span id="workflow_complete">', '')
+            tmp_2 = tmp_2.replace('</span>', '').strip()
+            final.append(tmp_1)
+            final.append(tmp_2)
+        elif 'CPU-Hours' in line:
+            tmp = lines[i+1].replace('<dd class="col-sm-9"><samp>', '')
+            tmp = tmp.replace('</samp></dd>', '').strip()
+            final.append(tmp+' hours')
+        elif 'Nextflow command' in line:
+            tmp = lines[i+1].replace('<dd><pre class="nfcommand"><code>', '')
+            tmp = tmp.replace('</code></pre></dd>', '').strip()
+            final.append(tmp)
+        elif 'Workflow execution' in line:
+            final.append(lines[i].strip())
+
+    return final
+
+
+class PDF(FPDF):
+    def titles(self, title, width=210, pos_x=0, pos_y=0):
+        self.set_xy(pos_x, pos_y)
+        self.set_font('Arial', 'B', 16)
+        self.multi_cell(w=width, h=20.0, align='C', txt=title,
+                        border=0)
+
+    def add_cell_left(self, title, text, size_y=10, width=200):
+        self.set_xy(5.0, self.get_y() + 4)
+        self.set_font('Arial', 'B', 12)
+        self.multi_cell(width, 5, align='L', txt=title)
+        self.set_xy(5.0, self.get_y())
+        self.set_font('Arial', '', 10)
+        self.multi_cell(width, size_y, align='L', txt=text, border=1)
+
+    def init_pos(self, pos_x=None, pos_y=None):
+        pos = [0, 0]
+        pos[0] = pos_x if pos_x is not None else 10
+        pos[1] = pos_y if pos_y is not None else self.get_y()+10
+        return pos
+
+    def add_image(self, title, filename, size_x=75, size_y=75,
+                  pos_x=None, pos_y=None):
+        pos = self.init_pos(pos_x, pos_y)
+        self.set_xy(pos[0], pos[1])
+        self.set_font('Arial', 'B', 12)
+        self.multi_cell(size_x, 5, align='C', txt=title)
+        self.image(filename, x=pos[0], y=pos[1]+5,
+                   w=size_x, h=size_y, type='PNG')
+        self.set_y(pos[1]+size_y+10)
+
+    def add_mosaic(self, main_tile, titles, filenames, size_x=75, size_y=75,
+                   row=1, col=1, pos_x=None, pos_y=None):
+        pos = self.init_pos(pos_x, pos_y)
+        self.set_xy(pos[0], pos[1])
+        self.set_font('Arial', 'B', 12)
+        self.multi_cell(size_x*col, 5, align='C', txt=main_tile)
+
+        for i in range(row):
+            for j in range(col):
+                self.set_xy(pos[0]+size_x*j, pos[1]+5+size_y*i)
+                self.set_font('Arial', '', 10)
+                self.multi_cell(size_x, 5, align='C', txt=titles[j+col*i])
+                self.image(filenames[j+col*i],
+                           x=pos[0]+size_x*j, y=pos[1]+10+size_y*i,
+                           w=size_x, h=size_y, type='PNG')
+        self.set_y(pos[1]+(size_y*row)+10)
+
+
+html_info = parse_report('report.html')
 METHODS = """TractoFlow [1] pipeline is developed by the Sherbrooke Connectivity Imaging Lab (SCIL) in order to process
 diffusion MRI dataset from the raw data to the tractography. The pipeline is based on Nextflow [2].
 
@@ -32,7 +107,7 @@ REFERENCES = """[1] Theaud, G., Houde, J.-C., Boré, A., Rheault, F., Morency, F
 [6] Jenkinson, M., Beckmann, C.F., Behrens, T.E., Woolrich, M.W., Smith, S.M., "Fsl." Neuroimage 62 (2012), 782-790.
 """
 
-pdf = pdf_creator.PDF(orientation='P', unit='mm', format='A4')
+pdf = PDF(orientation='P', unit='mm', format='A4')
 pdf.add_page()
 pdf.titles('Tractoflow_V1: {}'.format(sys.argv[1]))
 pdf.add_cell_left('Status:', html_info[0], size_y=5)
--- code/launch_tractoflow_wrapper.sh
+++ code/launch_tractoflow_wrapper.sh
@@ -2,7 +2,7 @@
 
 # Prepare input for Tractoflow
 IN_DIR=${1}
-OUT_DIR={2}
+OUT_DIR=${2}
 N_SUBJ=${3}
 N_SESS=${4}
 IN_T1=${5}
@@ -29,7 +29,7 @@ ALGO=${18}
 	--input raw/ --dti_shells "${DTI_SHELLS}" --fodf_shells "${FODF_SHELLS}" --run_dwi_denoising false --run_topup false \
 	--run_eddy false --use_slice_drop_correction false --mean_frf false --sh_order ${SH_ORDER} --run_pft_tracking ${RUN_PFT} \
 	--pft_algo ${ALGO} --pft_seeding_mask_type ${PFT_MASK_TYPE} \
-	--pft_seeding_mask_type interface --pft_nbr_seeds ${PFT_SEED} --run_local_tracking  --local_algo ${ALGO} \
+	--pft_seeding_mask_type interface --pft_nbr_seeds ${PFT_SEED} --run_local_tracking ${RUN_LOCAL} --local_algo ${ALGO} \
 	--local_tracking_mask_type ${LOCAL_MASK_TYPE} --local_nbr_seeds ${LOCAL_SEED} --processes_denoise_dwi 1 --processes_denoise_t1 1 \
 	--processes_eddy 1 --processes_fodf 1 --processes 1 --processes_brain_extraction_t1 1 --processes_registration 1 \
 	-resume -with-report report.html
@@ -44,25 +44,24 @@ convert pft.png -crop 600x3600+600+0 pft.png
 convert local.png -crop 600x3600+600+0 local.png
 
 xvfb-run -a --server-num=$((65536+$$)) --server-args="-screen 0 1600x1280x24 -ac" \
-	scil_visualize_fodf.py results/me/FODF_Metrics/me__fodf.nii.gz --axis_name axial --output axial.png --silent \
-	--background results/me/DTI_Metrics/me__fa.nii.gz --win_dims 2560 2560
+	scil_visualize_fodf.py results/*/FODF_Metrics/*__fodf.nii.gz --axis_name axial --output axial.png --silent \
+	--background results/*/DTI_Metrics/*__fa.nii.gz --win_dims 2560 2560
 xvfb-run -a --server-num=$((65536+$$)) --server-args="-screen 0 1600x1280x24 -ac" \
-	scil_visualize_fodf.py results/me/FODF_Metrics/me__fodf.nii.gz --axis_name coronal --output coronal.png --silent \
-	--background results/me/DTI_Metrics/me__fa.nii.gz --win_dims 2560 2560
+	scil_visualize_fodf.py results/*/FODF_Metrics/*__fodf.nii.gz --axis_name coronal --output coronal.png --silent \
+	--background results/*/DTI_Metrics/*__fa.nii.gz --win_dims 2560 2560
 
-/CODE/generate_tractoflow_spider_pdf.py ${N_SUBJ}_${N_SESS} 
+python3.7 /CODE/generate_tractoflow_spider_pdf.py ${N_SUBJ}_${N_SESS} 
 
-outputs:
 cp report.pdf report.html results/*/readme.txt ${OUT_DIR}/
-cp -L results/*/*__b0_resampled.nii.gz results/*/*__b0_mask_resampled.nii.gz results/*/*__dwi_resampled.nii.gz ${OUT_DIR}/
-cp -L results/*/*__output0GenericAffine.mat results/*/*__output1InverseWarp.nii.gz results/*/*__output1Warp.nii.gz ${OUT_DIR}/
-cp -L results/*/*__t1_mask_warped.nii.gz results/*/*__t1_warped.nii.gz ${OUT_DIR}/
-cp -L results/*/*__map_csf.nii.gz results/*/*__map_gm.nii.gz results/*/*__map_wm.nii.gz ${OUT_DIR}/
-cp -L results/*/*__mask_csf.nii.gz results/*/*__mask_gm.nii.gz results/*/*__mask_wm.nii.gz ${OUT_DIR}/
-cp -L results/*/*__local_seeding_mask.nii.gz results/*/*__local_tracking_mask.nii.gz results/*/*__pft_seeding_mask.nii.gz  ${OUT_DIR}/
-cp -L results/*/*__map_exclude.nii.gz results/*/*__map_include.nii.gz results/*/*__interface.nii.gz ${OUT_DIR}/
-cp -L results/*/*__ad.nii.gz results/*/*__rd.nii.gz results/*/*__fa.nii.gz results/*/*__rgb.nii.gz results/*/*__md.nii.gz ${OUT_DIR}/
-cp -L results/*/*__tensor.nii.gz results/*/*__evecs_v1.nii.gz results/*/*__evals_e1.nii.gz ${OUT_DIR}/
-cp -L results/*/*__afd_max.nii.gz results/*/*__afd_sum.nii.gz results/*/*__afd_total.nii.gz results/*/*__nufo.nii.gz ${OUT_DIR}/
-cp -L results/*/*__peaks.nii.gz results/*/*__fodf.nii.gz ${OUT_DIR}/
-cp -L results/*/*__local.trk results/*/*__pft.trk ${OUT_DIR}/
+cp -L results/*/*/*__b0_resampled.nii.gz results/*/*/*__b0_mask_resampled.nii.gz results/*/*/*__dwi_resampled.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__output0GenericAffine.mat results/*/*/*__output1InverseWarp.nii.gz results/*/*/*__output1Warp.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__t1_mask_warped.nii.gz results/*/*/*__t1_warped.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__map_csf.nii.gz results/*/*/*__map_gm.nii.gz results/*/*/*__map_wm.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__mask_csf.nii.gz results/*/*/*__mask_gm.nii.gz results/*/*/*__mask_wm.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__local_seeding_mask.nii.gz results/*/*/*__local_tracking_mask.nii.gz results/*/*/*__pft_seeding_mask.nii.gz  ${OUT_DIR}/
+cp -L results/*/*/*__map_exclude.nii.gz results/*/*/*__map_include.nii.gz results/*/*/*__interface.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__ad.nii.gz results/*/*/*__rd.nii.gz results/*/*/*__fa.nii.gz results/*/*/*__rgb.nii.gz results/*/*/*__md.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__tensor.nii.gz results/*/*/*__evecs_v1.nii.gz results/*/*/*__evals_e1.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__afd_max.nii.gz results/*/*/*__afd_sum.nii.gz results/*/*/*__afd_total.nii.gz results/*/*/*__nufo.nii.gz ${OUT_DIR}/
+cp -L results/*/*/*__peaks.nii.gz results/*/*/*__fodf.nii.gz ${OUT_DIR}/
+cp -L results/*/Local_Tracking/*.trk results/*/PFT_Tracking/*.trk ${OUT_DIR}/
